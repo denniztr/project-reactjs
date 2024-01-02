29eb06d403c4651b21f7e415eed80bc964354e7f
denniztr <denniztr@gmail.com>
2024-01-02 14:48:51 +0300

    adv-api init

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { AppRoutes } from "./routes/routes";
 
-import { Header, Authorization } from "./components";
+import { Header, Authorization, NewAdvModal } from "./components";
 
 function App() {
   const auth_modal = useSelector((state) => state.modal.authModal);
+  const adv_modal = useSelector((state) => state.modal.advModal);
   return (
     <BrowserRouter>
       <Header />
       <AppRoutes />
       { auth_modal ? <Authorization /> : null }
+      { adv_modal ? <NewAdvModal /> : null }
     </BrowserRouter>
   )
 }
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,28 +1,38 @@
-import { CiImageOff } from "react-icons/ci";
+/* eslint-disable react/prop-types */
+import { LiaTruckLoadingSolid } from "react-icons/lia";
+
 import './cards.scss';
 
-export const CardsContent = () => {
+export const CardsContent = ({ data, isLoading }) => {
+  console.log(isLoading)
+  data && console.log(data)
+
   return (
     <div className="main__content">
       <div className="content__cards cards">
-        <div className="cards__item">
-          <div className="cards__card card">
-            <a>
-              <div className="card__image">
-                {/* <img alt=""/> */}
-                <CiImageOff style={{'position': 'absolute', 'top': '45%', 'left': '45%'}}/>
-              </div>
-            </a>
-            <div className="card__content">
-              <a href="">
-                <h3 className="card__title">Заголовок</h3>
+        { isLoading ? (
+          <LiaTruckLoadingSolid size={100} className="loader"/>
+        ) : (
+          data.map((card) => (
+            <div className="cards__item" key={card.id}>
+            <div className="cards__card card">
+              <a>
+                <div className="card__image">
+                  <img alt="" src={`http://localhost:8090/${card.images[0]?.url}`}/>
+                </div>
               </a>
-              <p className="card__price">5000 рублей</p>
-              <p className="card__place">Санкт Петербург</p>
-              <p className="card__date">15 декабря 2023</p>
+              <div className="card__content">
+                <a href="">
+                  <h3 className="card__title">{card.title}</h3>
+                </a>
+                <p className="card__price">{card.price} ₽</p>
+                <p className="card__place">{card.user.city}</p>
+                <p className="card__date">{card.created_on}</p>
+              </div>
             </div>
           </div>
-        </div>
+          ))
+        ) }
       </div>
     </div>
   );
